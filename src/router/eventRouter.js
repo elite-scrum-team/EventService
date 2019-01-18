ba634edbeae44eb34bc9178d4b2b55d20717f231
William Chakroun Jacobsen <williacj@stud.ntnu.no>
2019-01-18 13:38:18 +0100

    i think it work!

---
+++ src/router/eventRouter.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const EventController = require('../controllers/eventController');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const result = await EventController.retrieve(req.query);
+    await res.send(result)
+});
+
+router.get('/:id', async (req, res) => {
+    const r = await EventController.retriveOne(req.params.id);
+    if (r) {
+        await res.send(r);
+    } else {
+        await res.send({ error: 'Warning with that id does not exist' }, 404);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    const result = await EventController.update(req.params.id, req.body);
+    if (result) {
+        await res.send(result)
+    } else {
+        await res.send({ error: 'Could not update warning' }, 500)
+    }
+});
+
+
+router.post('/', async (req, res) => {
+    const instanceOrError = await EventController.create(req.body);
+    await res.send(instanceOrError);
+});
+
+
+
+module.exports = router;
