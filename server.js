033cdc1fe02cf61a46d7c0c787460e4f4b443dfc
William Chakroun Jacobsen <williacj@stud.ntnu.no>
2019-01-18 11:58:14 +0100

    added multible js files.

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const client = require('prom-client');
+require('dotenv').config();
 
 const app = express();
+const port = process.env.port || 4000;
 
-app.get('/', async (req, res) => {
-    await res.send('vim > emax'); 
+// middleware
+app.use(bodyParser.json());
+
+// metrics
+client.collectDefaultMetrics({ timeout: 5000 });
+
+app.get('/metrics', async (req, res) => {
+    await res.set('Content-Type', client.register.contentType);
+    await res.end(client.register.metrics());
 });
 
-const port = process.env.port | 80;
+// Logging
+app.use(morgan('dev'));
 
-app.listen(80, () => console.log(`listening on port ${port}`));
+// routers
+app.use('/api/v1/image', require('./routers/image'));
+app.use('/api/v1/event', require('./routers/event'));
+
+app.get('/', async (req, res) => {
+    await res.send({
+        message: 'hello world!'
+    });
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
---
+++ src/util/services.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch');
+const querystring = require('querystring');
+
+module.exports = {
+    fetch: {
+        async get(serviceName, path, query) {
+            if (userId) query['internalUserId'] = userId;
+
+            let url = `http://${
+                process.env[serviceName.toUpperCase() + '_SERVICE_SERVICE_HOST']
+                }/api/v1/${path}`;
+
+            const qs = querystring.stringify(query);
+
+            if (qs) url += `?${qs}`;
+
+            return await fetch(url, {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+            });
+        },
+    },
+};
