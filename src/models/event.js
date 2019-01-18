6a773016ae947eeda43606f61a322bcfd917f024
William Chakroun Jacobsen <williacj@stud.ntnu.no>
2019-01-18 11:58:07 +0100

    added multible js files.

---
+++ src/models/event.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    const Event = sequelize.define('event', {
+        id: {
+            primaryKey: true,
+            type: DataTypes.UUID,
+            defaultValue: Sequelize.UUIDV4,
+        },
+        description: {
+            type: DataTypes.TEXT,
+        },
+        link: {
+            type: DataTypes.TEXT,
+        },
+        fromTime: {
+            type: DataTypes.DATE
+        },
+        toTime: {
+            type: DataTypes.DATE
+        },
+        locationID: {
+            type: DataTypes.UUID
+        }
+    });
+    Event.associate = models => {
+        Event.hasMany(models.image)
+    };
+    return Event;
+};
